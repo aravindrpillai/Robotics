1bee26ab376e862c3183b1238d4f3c5ddfef149d
Aravind R Pillai <aravindrpillai1992@gmail.com>
2019-02-13 23:23:35 +0530

    Update edge_avoider_atmega8_mini.c

--- edge_avoider_atmega8_mini.c
+++ edge_avoider_atmega8_mini.c
@@ -1,28 +1,3 @@
-// Project name : Edge avoider  for Atmega8 mini
-// Compile Date : 31/10/2011 Time : (16:21)
-// Designed By  : ROBOSAPIENS INDIA 
-//                http://www.robosapiensindia.com
-/* Program for "Edge avoider" 
- ___________________________________________________
-Connection settings of Kit
-
-LEDs---@------------->PB1
-LEDs---@------------->PB2
-LEDs---@------------->PB3
-LEDs---@------------->PB4
-RIGHT MOTOR(+)------->PB1
-RIGHT MOTOR(-)------->PB2
-LEFT MOTOR(-)-------->PB3
-LEFT MOTOR(+)-------->PB4
-BOOTLooder Condition Check----->PC5(f 0 bootler section else program execution
-							  section of Flash memory)
-RESET-------------------------->PC6
-Crystal Ossilator-------------->PB6 and PB7
-VB=Battery Supply
-VCC=regulated 5V+
-Gnd=Ground(0V)
-*/
-
 #include<avr/io.h>
 #include<util/delay.h>
 
